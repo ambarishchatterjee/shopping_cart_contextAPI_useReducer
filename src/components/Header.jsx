6ba48128cd48aa68b4cd8c936f601dba4a2a7008
ambarishchatterjee <amby008@gmail.com>
2025-02-27 09:40:38 +0530

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ export default function Header() {
       <header id="main-header">
         <div id="main-title">
           <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
+          <h1>Elegant Context Store</h1>
         </div>
         <p>
           <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
